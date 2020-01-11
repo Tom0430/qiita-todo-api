82387da36e801d49c421e7bd39a8efdc66588bda
tomoya takahashi <tiger@tomoyanoMacBook-Pro.local>
2020-01-11 11:32:52 +0900

    cors再設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    ## origins '*'
+    if Rails.env.production?
+        origins 'https://qiita-todo-3584d.firebaseapp.com/'
+    else
+        origins 'http://localhost:3000'
+    end
 
     resource '*',
       headers: :any,
